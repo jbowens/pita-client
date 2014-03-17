9d9ca29f47f8461105b221de93651cc2a71a44f3
Jacob Stern <jake@Jacob-Sterns-MacBook-Pro.local>
2014-03-16 21:49:57 -0400

    Add PTScrubViewController to view hierarchy

---
+++ MyLittlePITA/PTScrubViewController.h
@@ -0,0 +1,14 @@
+//
+//  PTScrubViewController.h
+//  MyLittlePITA
+//
+//  Created by Jacob Stern on 3/16/14.
+//  Copyright (c) 2014 My Little PITA Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface PTScrubViewController : GLKViewController
+
+@end
---
+++ MyLittlePITA/PTScrubViewController.m
@@ -0,0 +1,13 @@
+//
+//  PTScrubViewController.m
+//  MyLittlePITA
+//
+//  Created by Jacob Stern on 3/16/14.
+//  Copyright (c) 2014 My Little PITA Group. All rights reserved.
+//
+
+#import "PTScrubViewController.h"
+
+@implementation PTScrubViewController
+
+@end
