4720f28ce7141ad13efbb1dd2f18d5c1a9ab3bef
Jacob Stern <jake@Jacob-Sterns-MacBook-Pro.local>
2014-03-18 01:46:51 -0400

    Basic scrubbing functionality

--- MyLittlePITA/PTScrubViewController.mm
+++ MyLittlePITA/PTScrubViewController.mm
@@ -40,6 +40,19 @@ static const char kTextureQuadFragmentSource[] =
     "   gl_FragColor =  texture2D(texture, uv);                 "
     "}                                                          ";
 
+static const char kScrubbingFragmentSource[] =
+    "precision mediump float;                                   "
+    "                                                           "
+    "varying vec2 uv;                                           "
+    "uniform sampler2D texture;                                 "
+    "uniform vec2 stroke;                                       "
+    "                                                           "
+    "void main() {                                              "
+    "   if (distance(stroke, uv) > .01) {                       "
+    "       gl_FragColor =  texture2D(texture, uv);             "
+    "   } else { gl_FragColor = vec4(0, 0, 0, 0); }             "
+    "}                                                          ";
+
 
 @interface PTScrubViewController () {
     EAGLContext *_context;
@@ -49,17 +62,20 @@ static const char kTextureQuadFragmentSource[] =
     
     GLKTextureInfo *_texture;
     
-    js::OpenGLShaderProgram _textureQuadProgram;
+    js::OpenGLShaderProgram _textureQuadProgram, _scrubbingProgram;
     
-    GLuint _framebuffer, _framebufferTexture, _colorRenderBuffer;
+    GLuint _framebuffer, _framebufferTexture;
+    GLuint _backFramebuffer, _backFramebufferTexture;
     
     BOOL _shouldRedrawFramebuffer;
+    
+    NSArray *_pendingTouches;
 }
 
 - (void)setupGL;
 - (void)teardownGL;
 
-- (void)drawTextureQuad;
+- (void)drawTextureQuadUsingProgram:(js::OpenGLShaderProgram *)program;
 
 @end
 
@@ -115,34 +131,74 @@ static const char kTextureQuadFragmentSource[] =
     
     if (_textureQuadProgram.initialize()) {
         std::string reason = _textureQuadProgram.getErrorLog();
-        DDLogError(@"Error creating scrub shader program: %s", reason.c_str());
+        DDLogError(@"Error creating RTT shader program: %s", reason.c_str());
         
         return;
     }
     
-    glGenFramebuffers(1, &_framebuffer);
-    glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
+    _scrubbingProgram.setVertexSource(kTextureQuadVertexSource);
+    _scrubbingProgram.setFragmentSource(kScrubbingFragmentSource);
     
-    glActiveTexture(GL_TEXTURE0);
-    glGenTextures(1, &_framebufferTexture);
-
-    glBindTexture(GL_TEXTURE_2D, _framebufferTexture); {
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _screenWidth, _screenHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-
-    } glBindTexture(GL_TEXTURE_2D, 0);
-
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _framebufferTexture, 0);
-     
-    GLenum framebufferStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-    if (framebufferStatus != GL_FRAMEBUFFER_COMPLETE) {
-        DDLogError(@"Error creating scrub framebuffer, failed with status 0x%04X", framebufferStatus);
+    if (_scrubbingProgram.initialize()) {
+        std::string reason = _scrubbingProgram.getErrorLog();
+        DDLogError(@"Error creating scrubbing shader program: %s", reason.c_str());
         
         return;
+    }
+    
+    GLuint framebuffers[2];
+    glGenFramebuffers(2, framebuffers);
+    _framebuffer = framebuffers[0];
+    _backFramebuffer = framebuffers[1];
+    
+    glActiveTexture(GL_TEXTURE0);
+    GLuint textures[2];
+    glGenTextures(2, textures);
+    _framebufferTexture = textures[0];
+    _backFramebufferTexture = textures[1];
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer); {
+
+        glBindTexture(GL_TEXTURE_2D, _framebufferTexture); {
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _screenWidth, _screenHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+
+        } glBindTexture(GL_TEXTURE_2D, 0);
+
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _framebufferTexture, 0);
+         
+        GLenum framebufferStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+        if (framebufferStatus != GL_FRAMEBUFFER_COMPLETE) {
+            DDLogError(@"Error creating scrub framebuffer, failed with status 0x%04X", framebufferStatus);
+            
+            return;
+        }
+    }
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, _backFramebuffer); {
+
+        glBindTexture(GL_TEXTURE_2D, _backFramebufferTexture); {
+            
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _screenWidth, _screenHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+            
+        } glBindTexture(GL_TEXTURE_2D, 0);
+        
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _backFramebufferTexture, 0);
+        
+        GLenum framebufferStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+        if (framebufferStatus != GL_FRAMEBUFFER_COMPLETE) {
+            DDLogError(@"Error creating back framebuffer, failed with status 0x%04X", framebufferStatus);
+            
+            return;
+        }
     }
     
     CHECK_GL_ERROR();
@@ -156,9 +212,9 @@ static const char kTextureQuadFragmentSource[] =
     CHECK_GL_ERROR();
 }
 
-- (void)drawTextureQuad {
-    JS_USING(&_textureQuadProgram) {
-        GLuint texture = _textureQuadProgram.getUniformLocation("texture");
+- (void)drawTextureQuadUsingProgram:(js::OpenGLShaderProgram *)program {
+    JS_USING(program) {
+        GLuint texture = program->getUniformLocation("texture");
         glUniform1i(texture, 0);
         
         // From http://stackoverflow.com/questions/13455590/opengl-es-2-0-textured-quad
@@ -175,8 +231,8 @@ static const char kTextureQuadFragmentSource[] =
             1.0, 0.0,
             1.0, 1.0 };
         
-        GLuint position = _textureQuadProgram.getAttributeLocation("position");
-        GLuint texcoord = _textureQuadProgram.getAttributeLocation("texcoord");
+        GLuint position = program->getAttributeLocation("position");
+        GLuint texcoord = program->getAttributeLocation("texcoord");
         
         // Setup buffer offsets
         glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), quadPositions);
@@ -206,21 +262,68 @@ static const char kTextureQuadFragmentSource[] =
             glBindTexture(GL_TEXTURE_2D, _texture.name);
             glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
             
-            [self drawTextureQuad];
+            [self drawTextureQuadUsingProgram:&_textureQuadProgram];
+        }
+        
+        for (NSValue *point in _pendingTouches) {
+            CGPoint cgPoint = point.CGPointValue;
+            
+            glViewport(0, 0, _screenWidth, _screenHeight);
+            
+            glBindTexture(GL_TEXTURE_2D, _framebufferTexture);
+            glBindFramebuffer(GL_FRAMEBUFFER, _backFramebuffer);
+            
+            JS_USING(&_scrubbingProgram) {
+                GLuint stroke = _scrubbingProgram.getUniformLocation("stroke");
+                glUniform2f(stroke, cgPoint.x / _screenWidth, 1.f - cgPoint.y / _screenHeight);
+            } JS_END_USING
+            
+            [self drawTextureQuadUsingProgram:&_scrubbingProgram];
+            
+            // Swap our framebuffers
+            GLuint backFramebuffer = _backFramebuffer;
+            _backFramebuffer = _framebuffer;
+            _framebuffer = backFramebuffer;
+            
+            GLuint backFramebufferTexture = _backFramebufferTexture;
+            _backFramebufferTexture = _framebufferTexture;
+            _framebufferTexture = backFramebufferTexture;
         }
         
         [view bindDrawable];
             
         glBindTexture(GL_TEXTURE_2D, _framebufferTexture);
         
-        [self drawTextureQuad];
-        
+        [self drawTextureQuadUsingProgram:&_textureQuadProgram];
+
         glBindTexture(GL_TEXTURE_2D, 0);
         
         _shouldRedrawFramebuffer = NO;
     }
-    
+
     CHECK_GL_ERROR();
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesMoved:touches withEvent:event];
+    
+    NSMutableArray *pendingTouches = [NSMutableArray new];
+    for (UITouch *touch in touches) {
+        [pendingTouches addObject:[NSValue valueWithCGPoint:[touch locationInView:self.view]]];
+    }
+    
+    _pendingTouches = pendingTouches;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    
+    NSMutableArray *pendingTouches = [NSMutableArray new];
+    for (UITouch *touch in touches) {
+        [pendingTouches addObject:[NSValue valueWithCGPoint:[touch locationInView:self.view]]];
+    }
+    
+    _pendingTouches = pendingTouches;
 }
 
 @end
