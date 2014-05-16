0b1afcdf12a3c1766593b4cc29d031088f28c20c
Flora Jin <florajin14@gmail.com>
2014-05-16 04:48:34 -0400

    added mm file

--- MyLittlePITA/HotPocketDetector/PTHotPocketDetector.mm
+++ MyLittlePITA/HotPocketDetector/PTHotPocketDetector.mm
@@ -7,16 +7,14 @@
 //
 
 #import "PTHotPocketDetector.h"
-#import "PTColorHistogram.h"
 #import <Foundation/Foundation.h>
-
-using namespace std;
+#include <string>
 
 @implementation PTHotPocketDetector
 
-string dataFile;
+std::string dataFile;
 
-- (id)init:(string) dataFile
+- (id)init:(std::string) dataFile
 {
     self = [super init];
     if (self) {
@@ -26,22 +24,8 @@ string dataFile;
 }
 
 - (bool)isHotPocket:(UIImage*)im {
-    PTColorHistogram* hist = [[PTColorHistogram alloc] init:im];
     float total_red = 0;
-    float total_green = 0;
-    float total_blue = 0;
-    for (int i=3*16; i<4*16; i++) {
-        total_red += hist.histogram[i];
-    }
-    
-//    total_red = hist.histogram[(4*16)-1];
-//    for (int i=3*8;i<4*8;i++) {
-//        total_green += hist.histogram[i];
-//    }
-//    for (int i=16;i<2*16;i++) {
-//        total_blue += hist.histogram[i];
-//    }
-    [hist delete];
+
     NSLog(@"red percent: %f\n", total_red);
     if (total_red > .25) {
         return true;
